013b9c5d8de2d5763388c47846088de678be54bf
Stanislav <stas.turchenko.99@gmail.com>
2022-03-27 17:13:15 +0300

    связывание биновÐ

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Needle7 {
+
+    private Deth8 deth8;
+
+    @Autowired
+    public void setDeth8(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
 
     @Override
     public String toString() {
